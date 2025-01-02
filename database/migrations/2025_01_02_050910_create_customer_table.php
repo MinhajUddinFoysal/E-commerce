b58a3d10f964cbf728c0c4296865b91a34b536b5
SheikhMinhajUddin <mdminhaj432407@gmail.com>
2025-01-02 11:26:44 +0600

    file added

---
+++ database/migrations/2025_01_02_050910_create_customer_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customer', function (Blueprint $table) {
+            $table->id('customerId');
+            $table->string('name', length: 30);
+            $table->string('email', length: 20);
+            $table->string('password', length: 15);
+            $table->string('address', length: 40);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customer');
+    }
+};
